f3b552767f0a478a441f117697021d80bcf98eed
DACRepair <DACRepair@gmail.com>
2018-11-30 09:38:22 -0500

    Update udplistener.py

--- udplistener.py
+++ udplistener.py
@@ -1,16 +1,14 @@
 import socket
 import os
 
-
 UDP_IP = "0.0.0.0"
-UDP_PORT = int(os.environ['UDPPORT'])
+UDP_PORT = int(os.getenv('UDPPORT', '5005'))
 
-print "Listening on UDP port",UDP_PORT
+print("Listening on UDP port " + str(UDP_PORT))
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print data
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    print(str(addr) + " | " + str(data))
